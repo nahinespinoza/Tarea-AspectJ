3cbb000d993b91fbf4ae71cc44e0e68961753697
Nathaly Crespo <npcrespo@espol.edu.ec>
2023-10-28 11:57:31 -0500

    app creada

---
+++ src/ObserverApp.java
@@ -0,0 +1,29 @@
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ObserverApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+    	try {
+			Parent root = FXMLLoader.load(getClass().getResource("observer.fxml"));
+			primaryStage.setScene(new Scene(root, 650,500));
+			primaryStage.setTitle("Tarea Aspectos");
+	        primaryStage.show();
+		} catch (IOException e) {
+			System.out.println("Error al crear la ventana.");
+			e.printStackTrace();
+			
+		}
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nathaly
+ *
+ */
+module Observer {
+}
